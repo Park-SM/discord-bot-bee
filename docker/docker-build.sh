5f73327e755018c827d61c0ac77f0f90e76a3923
SangMin Park <park97.sm@gmail.com>
2024-10-15 02:08:44 +0900

    Update docker-build.sh

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -2,4 +2,4 @@ docker build -t smparkworld/discord-bot-bee:1.0 \
   --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
   --build-arg VERSION=1.0 \
   --build-arg BOT_TOKEN=$BOT_TOKEN \
-  ../../
+  ../
